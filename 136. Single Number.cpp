b56a6508fc8b536a0b435b3020d8660f08a2b724
spadek67424 <spadek67424@gmail.com>
2019-06-06 17:18:13 +0800

    Update 136. Single Number.cpp

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> ma;
         vector<int>::iterator it;
-        for(it=nums.begin();it!=nums.end();it++)
-        {
-            map<int,int>::iterator it2;
-            it2=ma.find(*it);
-            if(it2==ma.end()){
-                
-                ma[*it]=1;
-            }
-            else{
-                ma.erase(it2);
-            }
+        int start=0;
+        for(it=nums.begin();it!=nums.end();it++){
+            start=start^(*it);
         }
-        return (ma.begin())->first;
+        return start;
     }
 };
