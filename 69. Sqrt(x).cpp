f9a03963b7fa3d4abd09e80361bf8506406d6b9a
spadek67424 <spadek67424@gmail.com>
2019-06-15 22:27:47 +0800

    aaaaa

---
+++ 69. Sqrt(x).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left=1, right=x;
+        int mid=0;
+        if(x==0){
+            return 0;
+        }
+        while(true){
+            mid=left+(right-left)/2;
+            if(mid>x/mid){
+                right=mid-1;
+            }
+            else{
+                if((mid+1)>x/(mid+1)){
+                    return mid;
+                }
+                left=mid+1;
+            }
+        }
+        return mid;
+    }
+};
