507e8d4029a9ff2da167d357fde079afd30b74d9
spadek67424 <spadek67424@gmail.com>
2019-06-20 17:01:59 +0800

    use O(n) to search

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i=0;
+    
+        for(i=0;i<nums.size();){
+            if (nums[i]<target){
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+        return i;
+        
+    }
+};
